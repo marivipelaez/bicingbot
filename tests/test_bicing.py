5f8eb3c4e7e163351d3d849d5ec88d4e2b325477
Rubén González Alonso <rgonalo@gmail.com>
2016-07-23 18:18:00 +0200

    Convert unittest/nose tests to pytest

--- tests/test_bicing.py
+++ tests/test_bicing.py
@@ -18,11 +18,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import logging
-import unittest
 
 import mock
+import pytest
 import requests_mock
-from nose.tools import assert_equal, assert_raises
 from requests import ConnectionError
 
 from app.bicing import Bicing, GET_STATION_URL, StationNotFoundError
@@ -37,45 +36,43 @@ mock_not_found_station = '{"error":"not found"}'
 mock_error_station = '{"error":"unknown error"}'
 
 
-class TestBicing(unittest.TestCase):
-    """
+@requests_mock.Mocker()
+def test_get_station(req_mock):
+    station_id = 154
+    expected_station = {'id': '154', 'type': 'BIKE', 'latitude': '41.402454', 'longitude': '2.201656',
+                        'streetName': 'Pujades', 'streetNumber': '191', 'altitude': '10', 'slots': '15',
+                        'bikes': '11', 'nearbyStations': '141, 153, 165, 382', 'status': 'OPN'}
+    req_mock.get(GET_STATION_URL.format(station_id), content=mock_found_station)
 
-    """
+    station = Bicing().get_station(station_id)
+    assert station == expected_station
 
-    @requests_mock.Mocker()
-    def test_get_station(self, req_mock):
-        station_id = 154
-        expected_station = {'id': '154', 'type': 'BIKE', 'latitude': '41.402454', 'longitude': '2.201656',
-                            'streetName': 'Pujades', 'streetNumber': '191', 'altitude': '10', 'slots': '15',
-                            'bikes': '11', 'nearbyStations': '141, 153, 165, 382', 'status': 'OPN'}
-        req_mock.get(GET_STATION_URL.format(station_id), content=mock_found_station)
 
-        station = Bicing().get_station(station_id)
-        assert_equal(station, expected_station)
+@requests_mock.Mocker()
+def test_get_station_not_found(req_mock):
+    station_id = 9999
+    req_mock.get(GET_STATION_URL.format(station_id), content=mock_not_found_station)
 
-    @requests_mock.Mocker()
-    def test_get_station_not_found(self, req_mock):
-        station_id = 9999
-        req_mock.get(GET_STATION_URL.format(station_id), content=mock_not_found_station)
+    with pytest.raises(StationNotFoundError) as excinfo:
+        Bicing().get_station(station_id)
+    assert str(excinfo.value) == 'Station {} not found'.format(station_id)
 
-        assert_raises(StationNotFoundError, Bicing().get_station, station_id)
 
-    @mock.patch('app.bicing.requests.get')
-    def test_get_station_url_error(self, req_get_mock):
-        station_id = 154
-        req_get_mock.side_effect = ConnectionError('exception error')
+@mock.patch('app.bicing.requests.get')
+def test_get_station_url_error(req_get_mock):
+    station_id = 154
+    req_get_mock.side_effect = ConnectionError('exception error')
 
-        with assert_raises(Exception) as context:
-            Bicing().get_station(station_id)
+    with pytest.raises(Exception) as excinfo:
+        Bicing().get_station(station_id)
+    assert str(excinfo.value) == 'exception error'
 
-        assert_equal(context.exception.message, 'exception error')
 
-    @requests_mock.Mocker()
-    def test_get_station_error(self, req_mock):
-        station_id = 8888
-        req_mock.get(GET_STATION_URL.format(station_id), content=mock_error_station)
+@requests_mock.Mocker()
+def test_get_station_error(req_mock):
+    station_id = 8888
+    req_mock.get(GET_STATION_URL.format(station_id), content=mock_error_station)
 
-        with assert_raises(Exception) as context:
-            Bicing().get_station(station_id)
-
-        assert_equal(context.exception.message, 'Station {} can not be read due to unknown error'.format(station_id))
+    with pytest.raises(Exception) as excinfo:
+        Bicing().get_station(station_id)
+    assert str(excinfo.value) == 'Station {} can not be read due to unknown error'.format(station_id)
