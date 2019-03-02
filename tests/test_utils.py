da12f2dd7153cb110f5296530071de90c77f914c
Marivi Pelaez <vpelalo@gmail.com>
2019-03-02 13:28:50 +0100

    Update bicing method to use new stations api

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -31,31 +31,44 @@ def test_normalize_command_name():
 
 
 def test_pad_number_one_digit():
-    assert pad_number('5') == '  5'
+    assert pad_number(5) == '  5'
 
 
 def test_pad_number_two_digits():
-    assert pad_number('10') == '10'
+    assert pad_number(10) == '10'
 
 
 def test_compact_address_short():
-    assert compact_address('Pujades') == 'Pujades'
+    assert compact_address('12 - PUJADES, 154') == 'PUJADES, 154'
+    assert compact_address('12 - PUJADES') == 'PUJADES'
 
 
 def test_compact_address_long():
-    assert compact_address('Passeig Sant Joan') == 'Passeig Sant J'
+    assert compact_address('34 - PASSEIG SANT JOAN, 23') == 'PASSEIG SANT J, 23'
+    assert compact_address('34 - PASSEIG SANT JOAN') == 'PASSEIG SANT J'
 
 
 def test_compact_address_carrer():
-    assert compact_address('Carrer Pujades') == 'Pujades'
+    assert compact_address('12 - CARRER PUJADES, 154') == 'PUJADES, 154'
+    assert compact_address('12 - C/ PUJADES, 154') == 'PUJADES, 154'
+    assert compact_address('12 - C/PUJADES, 154') == 'PUJADES, 154'
 
 
 def test_compact_address_de():
-    assert compact_address('Carrer de Pujades') == 'Pujades'
+    assert compact_address('144 - CARRER DE DOCTOR TRUETA, 222') == 'DOCTOR TRUETA, 222'
 
 
 def test_compact_address_del():
-    assert compact_address('Ronda del Mig') == 'Ronda Mig'
+    assert compact_address('2 - RONDA DEL MIG, 220') == 'RONDA MIG, 220'
+
+
+def test_compact_address_additional():
+    assert compact_address('153 - C/ PUJADES, 173/RAMBLA DEL POBLENOU') == 'PUJADES, 173'
+    assert compact_address('153 - C/ PUJADES, 173 (RAMBLA DEL POBLENOU)') == 'PUJADES, 173'
+
+
+def test_compact_address_unicode():
+    assert compact_address('153 - RAMÓN TURRÓ, 154') == 'RAMÓN TURRÓ, 154'
 
 
 def test_grouper():
